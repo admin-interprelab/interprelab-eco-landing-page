e774497ae6443ffaf632e46982550552d88bd35c
Eladio Limones <admin.ceo@interprelab.com>
2025-10-11 07:47:40 -0600

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,6 @@ import { ProductShowcase } from "@/components/ProductShowcase";
 import { Resources } from "@/components/Resources";
 import { Testimonials } from "@/components/Testimonials";
 import { InterpreBotUI } from "@/components/ExtensionUI";
-import { InterpreCoachButton } from "@/components/InterpreCoachButton";
 import { Footer } from "@/components/Footer";
 
 const Index = () => {
@@ -21,11 +20,10 @@ const Index = () => {
       
       {/* InterpreBot - AI Assessment (First Contact) */}
       <InterpreBotUI />
-      
-      {/* InterpreCoach Extension Button */}
-      <InterpreCoachButton />
     </div>
   );
 };
 
 export default Index;
+
+
